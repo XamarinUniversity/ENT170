ec7711c5c594f16102652239e20bf54fe02b1ee4
Adam Patridge <patridge@gmail.com>
2017-09-05 11:38:12 -0600

    Initial course exercises import.

---
+++ Exercise 1/Start/Diary/Diary/AccountManager.cs
@@ -0,0 +1,46 @@
+﻿using Xamarin.Auth;
+using System.Linq;
+using System;
+
+namespace Diary.Shared
+{
+	public class AccountManager
+	{
+		const string serviceID = "Diary";
+		const string pwKey = "password";
+		const string kmKey = "keymaterial";
+		const string saltKey = "salt";
+
+ 		public bool CreateAndSaveAccount (string username, string password)
+		{
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                return false;
+
+			//TODO
+			return true;
+		}
+
+		public bool LoginToAccount (string username, string password)
+		{
+			if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+				return false;
+
+			//TODO
+			return true;
+		}
+	
+		public Account GetAccount (string username)
+		{
+			return GetAccountFromStore (AccountStore.Create (), username);
+		}
+
+		Account GetAccountFromStore (AccountStore store, string username)
+		{
+			if (store == null || username == null)
+				return null;
+
+            //TODO
+			return null;
+		}
+	}
+}
---
+++ Exercise 3/Completed/Diary/Diary.UWP/FileHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Diary.UWP
+{
+    public class FileHelper
+    {
+        public static string GetLocalStoragePath()
+        {
+            return Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+        }
+    }
+}
